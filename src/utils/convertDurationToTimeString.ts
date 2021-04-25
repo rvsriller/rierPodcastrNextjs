769fae8c1edf701284ac540c0c25b41bf69a82be
Riller <rvsriller@gmail.com>
2021-04-25 02:33:43 -0300

    Podcastr App

--- src/utils/convertDurationToTimeString.ts
+++ src/utils/convertDurationToTimeString.ts
@@ -1,6 +1,6 @@
 export function convertDurationToTimeString(duration: number) {
   const hours = Math.floor(duration / 3600);
-  const minutes = Math.floor(duration % 3600);
+  const minutes = Math.floor((duration % 3600) / 60);
   const seconds = duration % 60;
 
   const timeString = [hours, minutes, seconds]
